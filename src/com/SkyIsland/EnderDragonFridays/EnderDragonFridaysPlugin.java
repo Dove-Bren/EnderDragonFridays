48bcac411aeee5a73dda52da7c4356f9c1ab042f
Skyler Manzanares <skymanzanares@hotmail.com>
2014-09-29 20:16:58 -0600

    Created plugin class. First Commit.

---
+++ src/com/SkyIsland/EnderDragonFridays/EnderDragonFridaysPlugin.java
@@ -0,0 +1,18 @@
+package com.SkyIsland.EnderDragonFridays;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EnderDragonFridaysPlugin extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	public void onLoad() {
+		
+	}
+}
