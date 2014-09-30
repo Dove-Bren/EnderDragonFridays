9709a159c683757951fc65b7fc7ffa7a7e7f241f
Skyler Manzanares <skymanzanares@hotmail.com>
2014-09-30 16:28:38 -0600

    Created a ChestContentGenerator, which generates a list of chests given the players and how much each contributed to the fight.

---
+++ src/com/SkyIsland/EnderDragonFridays/Items/ChestContentGenerator.java
@@ -0,0 +1,26 @@
+package com.SkyIsland.EnderDragonFridays.Items;
+
+import java.util.Map;
+import org.bukkit.entity.Player;
+import org.bukkit.block.Chest;
+
+/**
+ * Generates a chest-ful of equipment for players!<br />
+ * Takes a mapping of players to their contribution and generates loot. The results are returned
+ * as a map between players and chests.<br />The chest the players are mapped to contain the loot
+ * generated for that player.
+ * @author Skyler
+ * @note This should be abstracted out when converting to a framework. Let people make their own ChestGenerators that don't,<br />
+ * for example, exclude 0% contribution
+ */
+public class ChestContentGenerator {
+	
+	public static Map<Player, Chest> generate(Map<Player, Double> inputMap) {
+		
+		for (Player player : inputMap.keySet()) {
+			//Create a chest
+		}
+		
+		return null;
+	}
+}
