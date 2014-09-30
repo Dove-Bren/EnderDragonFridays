7dddc6390802e95ebc050de76971b3d24dbd9b3b
Skyler Manzanares <skymanzanares@hotmail.com>
2014-09-30 10:45:38 -0600

    Began adding specifics, but didn't get too far

--- src/com/SkyIsland/EnderDragonFridays/EnderDragon.java
+++ src/com/SkyIsland/EnderDragonFridays/EnderDragon.java
@@ -3,16 +3,25 @@ package com.SkyIsland.EnderDragonFridays;
 import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
 
-public class EnderDragon {
+public class EnderDragon implements Listener {
 	private LivingEntity dragon;
+	private EnderDragonFridaysPlugin plugin;
 	
 	/**
-	 * Creates an enderdragon
+	 * Creates a default enderdragon
 	 * @param loc
 	 * @param name it's name
 	 */
-	public EnderDragon(Location loc, String name) {
+	public EnderDragon(EnderDragonFridaysPlugin plugin, Location loc, String name) {
+		this.plugin = plugin;
+		
+		//register this as an event listener
+		plugin.getServer().getPluginManager().registerEvents(this,plugin);
+		
 		dragon = (LivingEntity) loc.getWorld().spawnEntity(loc, EntityType.ENDER_DRAGON);
 		if (name != null && name.length() > 0) {
 			dragon.setCustomName(name);
@@ -20,7 +29,9 @@ public class EnderDragon {
 		}
 	}
 	
-	
+	public EnderDragon(EnderDragonFridaysPlugin plugin, LivingEntity dragon) {
+		this.dragon = dragon;
+	}
 	
 	
 	public boolean isLiving() {
@@ -29,5 +40,12 @@ public class EnderDragon {
 		}
 
 		return !dragon.isDead();
+	}
+	
+	@EventHandler
+	public void dragonDeath(EntityDeathEvent event) {
+		if (event.getEntity().equals(dragon)) {
+			//if the dragon has died
+		}
 	}
 }
--- src/com/SkyIsland/EnderDragonFridays/EnderDragonFridaysPlugin.java
+++ src/com/SkyIsland/EnderDragonFridays/EnderDragonFridaysPlugin.java
@@ -2,6 +2,8 @@ package com.SkyIsland.EnderDragonFridays;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -25,8 +27,10 @@ public class EnderDragonFridaysPlugin extends JavaPlugin {
 		
 		if (cmd.getName().equalsIgnoreCase("makestuff"))
 		if (dragon.isLiving()){
-			dragon = new EnderDragon(( (Player) sender).getLocation().add(0, 30, 0), "Dragon");
-			return true;
+			LivingEntity drags;
+			drags = (LivingEntity) ((Player) sender).getWorld().spawnEntity(((Player) sender).getLocation().add(0, 20, 0), EntityType.ENDER_DRAGON);
+			drags.setMaxHealth(((Player) sender).getWorld().getPlayers().size() * drags.getMaxHealth()); //scale up with players
+			drags.setCustomName("Young Ender Dragon");
 		}
 		
 		
