245c80635cb511d336c794d0503736dc42852cf5
Skyler Manzanares <skymanzanares@hotmail.com>
2014-09-30 16:12:18 -0600

    Fleshed out BossNameGenerator and added comments. Created ItemNameGenerator, which will create names for items.

--- src/com/SkyIsland/EnderDragonFridays/Items/Name/BossNameGenerator.java
+++ src/com/SkyIsland/EnderDragonFridays/Items/Name/BossNameGenerator.java
@@ -10,21 +10,30 @@ public class BossNameGenerator implements NameGenerator {
 	private List<String> firstNames;
 	private List<String> lastNames;
 	
+	/**
+	 * Creates a name generator ready to spit out awesome boss names!
+	 */
 	public BossNameGenerator() {
 		rand = new Random();
-		firstNames = new ArrayList<String>();
+		firstNames = new ArrayList<String>(); //we use array lists for fast random access
 		lastNames = new ArrayList<String>();
 		
-		setupFirst();
-		setupLast();
+		setupFirst(); //load up first names
+		setupLast(); //load up last names
 		
 	}
-	
+	/**
+	 * Returns a boss name.<br />
+	 * This is a complete name, composed of a first name and a last name separated by a space.
+	 * <p />
+	 * This is the same as 
+	 *        getFirstName() + " " + getLastName()
+	 */
 	@Override
 	public String getName() {
 		return (getFirstName() + " " + getLastName());
 	}
-
+	
 	@Override
 	public String getFirstName() {
 		return firstNames.get(rand.nextInt(firstNames.size()));
@@ -35,6 +44,9 @@ public class BossNameGenerator implements NameGenerator {
 		return lastNames.get(rand.nextInt(lastNames.size()));
 	}
 	
+	/**
+	 * Add our first names to a list
+	 */
 	private void setupFirst() {
 		firstNames.add("Uldar");
 		firstNames.add("Bred");
@@ -59,6 +71,9 @@ public class BossNameGenerator implements NameGenerator {
 		firstNames.add("Wyste");
 	}
 	
+	/**
+	 * Add all our last names to a list
+	 */
 	private void setupLast() {
 		lastNames.add("Bonecrusher");
 		lastNames.add("Stoneweaver");
