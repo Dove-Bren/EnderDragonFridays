8c2dca078322127e31aff5d7cce06db19317c257
Skyler Manzanares <skymanzanares@hotmail.com>
2014-09-30 18:34:29 -0600

    Filled getMostDamage() event of Ender Dragon, which returns the player that's done the most damage. Also added a cannon that's created.

--- src/com/SkyIsland/EnderDragonFridays/EnderDragon.java
+++ src/com/SkyIsland/EnderDragonFridays/EnderDragon.java
@@ -2,6 +2,7 @@ package com.SkyIsland.EnderDragonFridays;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
@@ -9,14 +10,13 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.inventory.Inventory;
 
 public class EnderDragon implements Listener {
 	private LivingEntity dragon;
 	private EnderDragonFridaysPlugin plugin;
-	private Map<Player, Integer> damageMap;
-	private Map<Player, Inventory> chestMap;
+	private Map<Player, Double> damageMap;
 	private FireballCannon cannon;
 	
 	/**
@@ -25,23 +25,35 @@ public class EnderDragon implements Listener {
 	 * @param name it's name
 	 */
 	public EnderDragon(EnderDragonFridaysPlugin plugin, Location loc, String name) {
-		this.plugin = plugin;
-		
-		//register this as an event listener
-		plugin.getServer().getPluginManager().registerEvents(this,plugin);
-		
+		this.setPlugin(plugin);
 		dragon = (LivingEntity) loc.getWorld().spawnEntity(loc, EntityType.ENDER_DRAGON);
 		if (name != null && name.length() > 0) {
 			dragon.setCustomName(name);
 			dragon.setCustomNameVisible(true);
 		}
+
+
+		//register this as an event listener
+		plugin.getServer().getPluginManager().registerEvents(this,plugin);
 		
-		damageMap = new HashMap<Player, Integer>();
-		chestMap = new HashMap<Player, Inventory>();
+		
+		damageMap = new HashMap<Player, Double>();
+		
+		cannon = new FireballCannon(this, 3000, 10000);
+		cannon.start();
+		plugin.getLogger().info("Ender Dragon Created!");
 	}
 	
 	public EnderDragon(EnderDragonFridaysPlugin plugin, LivingEntity dragon) {
 		this.dragon = dragon;
+
+		//register this as an event listener
+		plugin.getServer().getPluginManager().registerEvents(this,plugin);
+		
+		
+		damageMap = new HashMap<Player, Double>();
+		cannon = new FireballCannon(this, 3000, 10000);
+		cannon.start();
 	}
 	
 	
@@ -58,7 +70,38 @@ public class EnderDragon implements Listener {
 	}
 	
 	public Player getMostDamage() {
-		return null;
+		if (damageMap.isEmpty()) {
+			return null;
+		}
+		
+		Player player = null;
+		double max = -999999.0;
+		for (Entry<Player, Double> entry : damageMap.entrySet()) {
+			if (entry.getValue() > max) {
+				player = entry.getKey();
+			}
+		}
+		
+		
+		return player;
+	}
+	
+	@EventHandler
+	public void dragonDamage(EntityDamageByEntityEvent event) {
+		if (event.getEntity().equals(dragon)) {
+			//if it was our dragon
+			if (event.getDamager() instanceof Player) {
+				if (!damageMap.containsKey((Player) event.getDamager())) {
+					//new player that's damaged it
+					damageMap.put((Player) event.getDamager(), event.getDamage() / dragon.getMaxHealth());
+				}
+				else {
+					double old = damageMap.get((Player) event.getDamager());
+					old = old + (event.getDamage() / dragon.getMaxHealth());
+					damageMap.put((Player) event.getDamager(), old); 
+				}
+			}
+		}
 	}
 	
 	@EventHandler
@@ -66,5 +109,13 @@ public class EnderDragon implements Listener {
 		if (event.getEntity().equals(dragon)) {
 			//if the dragon has died
 		}
+	}
+
+	public EnderDragonFridaysPlugin getPlugin() {
+		return plugin;
+	}
+
+	public void setPlugin(EnderDragonFridaysPlugin plugin) {
+		this.plugin = plugin;
 	}
 }
