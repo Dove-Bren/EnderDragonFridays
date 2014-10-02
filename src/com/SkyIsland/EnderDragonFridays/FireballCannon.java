4bb1e1bcaa8573306e98b24fc694c892b547149e
m0pt0pmatt <m0pt0pmatt17@gmail.com>
2014-10-02 14:13:20 -0600

    fixed spelling error

--- src/com/SkyIsland/EnderDragonFridays/FireballCannon.java
+++ src/com/SkyIsland/EnderDragonFridays/FireballCannon.java
@@ -13,7 +13,7 @@ import org.bukkit.util.Vector;
 /**
  * 
  * @author Skyler
- * @note This could be extracted out to 'body part' class. There oculd be two. One that's independent and one that just
+ * @note This could be extracted out to 'body part' class. There could be two. One that's independent and one that just
  * 			exists. I.e. one that extends thread and one that doens't get it's own.
  */
 public class FireballCannon extends Thread {
