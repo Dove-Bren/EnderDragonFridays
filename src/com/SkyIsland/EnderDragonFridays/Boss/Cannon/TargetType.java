8989cdfc5ee362c277a43aa01b08cb3295c19910
Skyler Manzanares <skymanzanares@hotmail.com>
2014-11-28 15:21:32 -0700

    Updated comments to document the two new types of targetting

--- src/com/SkyIsland/EnderDragonFridays/Boss/Cannon/TargetType.java
+++ src/com/SkyIsland/EnderDragonFridays/Boss/Cannon/TargetType.java
@@ -4,8 +4,10 @@ package com.SkyIsland.EnderDragonFridays.Boss.Cannon;
  * Types of targeting methods used in cannons.
  * 
  * <p><ul>
- * <li><i>NEAREST</i>: attack the closest valid target</li>
- * <li><i>MOSTDAMAGE</i>: attack the entity that has done the most damage</li>
+ * <li><b>NEAREST</b>: attack the closest valid target</li>
+ * <li><b>MOSTDAMAGE</b>: attack the entity that has done the most damage</li>
+ * <li><b>RANDOM</b>: pick a player in the world at random to attack</li>
+ * <li><b>ALL_CYCLE</b>: attack all players in the world, one at a time in a cyclic fashion</li> 
  * </ul></p>
  * @author Skyler
  *
