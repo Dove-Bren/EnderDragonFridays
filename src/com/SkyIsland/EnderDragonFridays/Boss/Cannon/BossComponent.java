bd6049b42a8b2258aa80865e26fe01d6ea197891
Skyler Manzanares <skymanzanares@hotmail.com>
2014-11-28 15:42:06 -0700

    Created a new generic BossComponent class. Cannon is below this. Gave Cannon variables and behaviors common to cannon types. Added a lot of sweet comments.

---
+++ src/com/SkyIsland/EnderDragonFridays/Boss/Cannon/BossComponent.java
@@ -0,0 +1,22 @@
+package com.SkyIsland.EnderDragonFridays.Boss.Cannon;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+import com.SkyIsland.EnderDragonFridays.Boss.Boss;
+
+/**
+ * A piece of a boss designed to run with little-to-no synchronization.<br />
+ * Components aren't designed to significantly alter a boss's behavior. Components, instead, should be
+ * things like cannons and status effect inducers that run independently of the boss. Communication between
+ * each component and the boss (Which runs on the main thread!!!) should be achieved through event handling
+ * and other similarly asynchronous methods.
+ * @author Skyler
+ *
+ */
+public abstract class BossComponent extends BukkitRunnable {
+	
+	/**
+	 * The {@link com.SkyIsland.EnderDragonFridays.Boss.Boss Boss} this component is part of.
+	 */
+	protected Boss boss;
+}
--- src/com/SkyIsland/EnderDragonFridays/Boss/Cannon/Cannon.java
+++ src/com/SkyIsland/EnderDragonFridays/Boss/Cannon/Cannon.java
@@ -1,7 +1,93 @@
 package com.SkyIsland.EnderDragonFridays.Boss.Cannon;
 
-import org.bukkit.scheduler.BukkitRunnable;
 
-public abstract class Cannon extends BukkitRunnable {
+public abstract class Cannon extends BossComponent {
+	
+	/**
+	 * The mode of selecting a target. See {@link com.SkyIsland.EnderDragonFridays.Boss.Cannon.TargetType TargetType}
+	 * for more info.
+	 */
+	protected TargetType targetType;
+	
+	/**
+	 * Stores the smallest amount of time inbetween cannon fires
+	 */
+	protected double delayMin;
+	
+	/**
+	 * Stores the largest amount of time inbetween cannon fires
+	 */
+	protected double delayMax;
+	
+	/**
+	 * When time-till-fire is calculated, we choose from a small set of integers instead of a continuous
+	 * range of floats. delay_incr keeps track of the smallest difference between calculated firing times
+	 * possible.
+	 */
+	protected double delayIncr;
+	
+	/**
+	 * Because we're using a subset of ints, we need a random number from a range to pick how long our
+	 * delay is actually going to be. This variable is here because we very frequently calculate<br /><br />
+	 * <center>round( [delay_max - delay_min] / delay_incr)</center>
+	 * <br />many times. To speed this up, we store it in a variable.<br />
+	 * <b>This is not a good solution, but I can't bring myself to calculate that every time. It's always
+	 * the same number!</b>
+	 */
+	protected int delayRange;
+	
+	/**
+	 * Returns the {@link com.SkyIsland.EnderDragonFridays.Boss.Cannon.TargetType TargetType} used when
+	 * selecting a target to fire on.
+	 * @return
+	 */
+	public TargetType getTargetType() {
+		return this.targetType;
+	}
+	
+	
+	/**
+	 * Returns the shortest amount of time a cannon can go between firing.
+	 * @return The time in ticks
+	 */
+	public double getDelayMin() {
+		return delayMin;
+	}
+
+
+	/**
+	 * Returns the longest amount of time a cannon can go between firing.
+	 * @return The time in ticks
+	 */
+	public double getDelayMax() {
+		return delayMax;
+	}
+
+
+	/**
+	 * Returns how big increments are between min and max times.
+	 * @return The time in ticks
+	 */
+	public double getDelayIncr() {
+		return delayIncr;
+	}
+
+
+
+	/**
+	 * Gives the cannon the passed method of selecting a target to fire on.
+	 * @param type Mode of selecting the target.
+	 * @see {@link com.SkyIsland.EnderDragonFridays.Boss.Cannon.TargetType TargetType}
+	 */
+	public void setTargetType(TargetType type) {
+		this.targetType = type;
+	}
+	
+	/*
+	 * setters for our delay variables intentionally left out!!!
+	 * values are to be calculated in the constructors of the cannons to follow the guidelines of the cannons.
+	 * This allows for the different delays corresponding to different aspects of the in-game objects to be
+	 * accounted for on an individual basis.
+	 */
 	
 }
